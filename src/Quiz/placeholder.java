6a390656f1e34085995918ab1e7b3ff0140fa60b
Andrejs Račkovs <piradzins8@gmail.com>
2024-06-05 00:28:13 +0300

    Prezentācija tuvu beigām

---
+++ src/Quiz/placeholder.java
@@ -0,0 +1,14 @@
+package Quiz;
+
+public class placeholder {
+	
+	 public static void main(String[] args)
+	    {
+	 
+	        int[][] A = {{ 4, 5, 8, 4},
+	        			 {6 , 7, 9, 0},
+	        			 {12, 22, 6, 2}};
+System.out.println("Masīva 2 Rindas 3 Kolonnas elements: "
+	        + A[1][2]); // Var Atrast konkrētu masīva vērtību
+	    }
+	}
