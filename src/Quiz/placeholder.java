1fade8bcf0496f16a91c4f5ce544f4881a0d3361
Andrejs Račkovs <piradzins8@gmail.com>
2024-06-08 00:03:23 +0300

    Prezentācija pabeigta

--- src/Quiz/placeholder.java
+++ src/Quiz/placeholder.java
@@ -1,14 +1,66 @@
 package Quiz;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class placeholder {
 	
-	 public static void main(String[] args)
-	    {
+    public static void main(String[] args) {
 	 
-	        int[][] A = {{ 4, 5, 8, 4},
-	        			 {6 , 7, 9, 0},
-	        			 {12, 22, 6, 2}};
-System.out.println("Masīva 2 Rindas 3 Kolonnas elements: "
-	        + A[1][2]); // Var Atrast konkrētu masīva vērtību
-	    }
-	}
+        int r, k, maxSS, pag, maxI = 0, maxJ = 0; 
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        
+        do {
+            System.out.print("Ievadi rindu skaitu: ");
+            r = scan.nextInt();
+            System.out.print("Ievadi kolonnu skaitu: ");
+            k = scan.nextInt();
+        } while (r < 2 || k < 2);
+        
+        int[][] A = new int[r][k];
+        int[][] B = new int[r][k];
+        int id = 0;
+        int[] pagMas = new int[r * k];
+        
+        System.out.println("Izveidotais masīvs A: ");
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                A[i][j] = rand.nextInt(100) + 1; 
+                B[i][j] = A[i][j];
+                System.out.print(A[i][j] + " ");
+                pagMas[id] = A[i][j];
+                id++;
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < pagMas.length - 1; i++) {
+            maxSS = i;
+            for (int j = i + 1; j < pagMas.length; j++) {
+                if (pagMas[j] > pagMas[maxSS])
+                    maxSS = j;
+            }
+            pag = pagMas[maxSS];
+            pagMas[maxSS] = pagMas[i];
+            pagMas[i] = pag;
+        }
+        
+        System.out.println("A sakārtots ar Selection Sort (dilstoši):");
+        id = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                A[i][j] = pagMas[id];
+                id++;
+                System.out.print(A[i][j] + " ");
+            }
+            System.out.println();
+        }
+        id = 0;
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[i].length; j++) {
+                pagMas[id] = B[i][j];
+                id++;
+            }
+        }
+            }
+    }
