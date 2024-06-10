ae9a8b10ffb23547b880ae15f9c8d369465165e0
Andrejs Račkovs <piradzins8@gmail.com>
2024-06-10 14:34:16 +0300

    Izmaiņas jautājumos lai var padot testam

--- src/Quiz/Info.java
+++ src/Quiz/Info.java
@@ -2,11 +2,9 @@ package Quiz;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
--- src/Quiz/Questons.java
+++ src/Quiz/Questons.java
@@ -1,28 +1,34 @@
 package Quiz;
 
 public class Questons {
-
-	public static void main(String[] args) {
-		
-
-		   String[][] Jautajumi = {
-		            {"Kā pareizi deklarēt divdimensiju masīvu Java?"},
-		            {"Kādas metodes var izmantot, lai inicializētu 3x3 divdimensiju masīvu Java?"},
-		            {"Kuras no šīm koda daļām pareizi izdrukā divdimensiju masīva elementus Java?"},
-		            {"Kuri no šiem apgalvojumiem par divdimensiju masīviem Java ir patiesi?"},
-		            {"Kuri no šiem apgalvojumiem kompilēsies bez kļūdām?"},
-		            {"Kādas metodes var izmantot, lai iegūtu divdimensiju masīva garumu Java?"},
-		            {"Kuri apgalvojumi par divdimensiju masīviem Java ir pareizi?"},
-		            {"Kādi ir pareizi veidi, kā piekļūt divdimensiju masīva 'arr' elementiem Java?"},
-		            {"Kā pareizi definēt un inicializēt divdimensiju String masīvu Java?"},
-		            {"Kuri no šiem apgalvojumiem par divdimensiju masīviem Java ir patiesi?"}
-		        };
-		
-		char[] Atbildes = { '1',
-							'2',
-							'3',
-							'4'
-		};
-	}
-
+	
+    public static String[][] getQuestions() {
+        return new String[][]{
+            {"Kā pareizi deklarēt divdimensiju masīvu Java?"},
+            {"Kādas metodes var izmantot, lai inicializētu 3x3 divdimensiju masīvu Java?"},
+            {"Kuras no šīm koda daļām pareizi izdrukā divdimensiju masīva elementus Java?"},
+            {"Kuri no šiem apgalvojumiem par divdimensiju masīviem Java ir patiesi?"},
+            {"Kuri no šiem apgalvojumiem kompilēsies bez kļūdām?"},
+            {"Kādas metodes var izmantot, lai iegūtu divdimensiju masīva garumu Java?"},
+            {"Kuri apgalvojumi par divdimensiju masīviem Java ir pareizi?"},
+            {"Kādi ir pareizi veidi, kā piekļūt divdimensiju masīva 'arr' elementiem Java?"},
+            {"Kā pareizi definēt un inicializēt divdimensiju String masīvu Java?"},
+            {"Kuri no šiem apgalvojumiem par divdimensiju masīviem Java ir patiesi?"}
+        };
+    }
+    
+    public static char[][] getAnswers() {
+        return new char[][]{
+            {'1', '4'},
+            {'1', '3'},
+            {'1', '2'},
+            {'2', '3'},
+            {'1', '3'},
+            {'1', '2'},
+            {'1', '2'},
+            {'2', '3'},
+            {'1', '4'},
+            {'1', '3'}
+        };
+    }
 }
